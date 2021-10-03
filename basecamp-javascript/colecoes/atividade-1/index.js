2357d8b4a9d2c8c8d886596516c52d776edd814f
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2021-10-03 08:57:31 -0300

    testing maps

---
+++ basecamp-javascript/colecoes/atividade-1/index.js
@@ -0,0 +1,15 @@
+/* 
+Atividade 1: Maps
+Crie uma função que retorna o nome dos membros de um Map 
+que tem o papel 'ADMIN' no sistema.
+*/
+
+const users = new Map()
+users.set('Oliver','regular');
+users.set('Margit','admin');
+users.set('regular', 'Clever', 'regular');
+users.set('Monty','admin');
+users.set('Seath','regular');
+users.set('Beaver','regular');
+
+
